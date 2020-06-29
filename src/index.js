e73927060a46dec5d212ad34c8dd1f7045735516
Jesse Kelly <kellyjesse881@gmail.com>
2020-06-29 13:51:38 -0500

    chore(testing): started example page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import moment from "moment";
+import { useInterval } from "react-use";
 
-export const TimeStr = props => <span {...props}>Time</span>;
+const getTimeStr = format => moment().format(format || "h:mm A");
+
+/**
+ * CurrentTimeStr
+ * Displays the current time as a string(in a span)
+ */
+export const CurrentTimeStr = ({ format, ...props }) => {
+    useInterval(_ => setTime(getTimeStr(format)), 1000);
+    const [time, setTime] = useState(getTimeStr(format));
+
+    return <span {...props}>{time}</span>;
+};
--- test/index.js
+++ test/index.js
@@ -1,16 +1,20 @@
-import { TimeStr } from "src";
+import { CurrentTimeStr } from "src";
 import ReactDOM from "react-dom";
 import React from "react";
-
-console.log(TimeStr);
 
 /**
  * Demo
  */
-export const Demo = props => (
+export const Demo = _ => (
     <>
         <h1>UI Utils Demo</h1>
-        <TimeStr />
+        <section>
+            <h2>DateTime Related</h2>
+            <section>
+                <h3>CurrentTimeStr</h3>
+                <CurrentTimeStr />
+            </section>
+        </section>
     </>
 );
 
